a9dab075d19971e0152b58370850f58d3f1b4bfa
Mathrix <68941653+Dreams-Possible@users.noreply.github.com>
2025-07-03 22:23:14 +0800

    Add files via upload

---
+++ ATKMS901M_Driver/sp_stand_start/main/te_uart_0.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//头文件
+#include"sp_sys.h"
+#include<stdio.h>
+#include<string.h>
+#include"driver/uart.h"
+
+//初始化串口
+uint8_t te_uart_0_init();
+//串口接收
+char*te_uart_0_rx();
+//串口接收字节
+void te_uart_0_rx_bin(u8**data,u16*len);
+//串口发送
+void te_uart_0_tx(char*data,...);
+//串口发送字节
+void te_uart_0_tx_bin(u8*data,u16 len);
